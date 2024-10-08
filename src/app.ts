ebfdd71a5d5e72bda086ac3d212cef5f2d52ccdc
Malcon Augusto Souza De Miranda <malcon.miranda@cea.com.br>
2024-10-08 14:01:01 -0300

    refactor: ajustando conexão entre containers

--- src/app.ts
+++ src/app.ts
@@ -11,32 +11,7 @@ app.register(cors, {
   origin: '*',
   methods: ['GET']
 })
-
-
-import { ConnectionPool } from "mssql";
-
-const config = {
-  user: "sa",
-  password: "@k]rHSdfbKW0",
-  server: "localhost,1433",
-  database: "tcc-impacta",
-  options: {
-    encrypt: false,
-    enableArithAbort: false,
-  },
-};
-
-const pool = new ConnectionPool(config);
-
-pool
-  .connect()
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.error("Database connection failed: ", err);
-  });
-
+// Global Error Handler
 app.setErrorHandler((err, _, reply) => {
   if (err instanceof ZodError) {
     return reply.status(400).send({ message: 'Validation error', issues: err.format() })
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,30 @@
 import { app } from './app'
+import { env } from 'process';
+import { ConnectionPool } from "mssql";
 
+const config = {
+  user: "sa",
+  password: "@k]rHSdfbKW0",
+  server: env.DATABASE_HOST, 
+  database: "tcc-impacta",
+  options: {
+    encrypt: false,
+    enableArithAbort: false,
+  },
+};
+
+const pool = new ConnectionPool(config);
 app.listen({
   host: '0.0.0.0',
   port: 7500
 }).then(() => {
   console.log('Server listening on port 7500')
+  pool
+    .connect()
+    .then(() => {
+      console.log("Connected to the database!");
+    })
+    .catch((err) => {
+      console.error("Database connection failed: ", err);
+    });
 })
